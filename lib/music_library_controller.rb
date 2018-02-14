5d26147756e310e12b203c40eb4d88c61b80950d
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-14 13:24:12 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -77,8 +77,8 @@ class MusicLibraryController
   end
 
   def play_song
-    self.list_songs
-    puts "Please enter the name of a genre:"
+    #self.list_songs
+    puts "Which song number would you like to play?"
     input = gets.strip
   end
 
