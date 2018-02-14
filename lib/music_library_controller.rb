e57d462baee7da33f70537779711b346bdf2522b
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-14 12:52:28 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,7 +45,7 @@ class MusicLibraryController
   end
 
   def list_artists
-    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(0) do |a, i|
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
       puts "#{i}. #{a.name}"
     end
   end
