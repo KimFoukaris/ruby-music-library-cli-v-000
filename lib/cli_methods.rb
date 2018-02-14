639ba5a17d64a902ebfcf3b98395f62eeea17051
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-14 10:58:20 +0000

    automatically backed up by learn

--- lib/cli_methods.rb
+++ lib/cli_methods.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 class CLI_methods
 
-  def initialize
-    music_library_controller = MusicLibraryController.new
-    binding.pry
+  def initialize(path)
+    music_library_controller = MusicLibraryController.new(path)
   end
 
   def list_songs
