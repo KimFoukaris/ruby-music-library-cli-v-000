ae3c7b5aa0b568a3804fd2efafd0bb72d2bcda47
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-14 10:38:22 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    input = ""
+    while input != "exit"
+      input = gets
+    end
   end
 
 end
